9af0490fe27183dc52ea2e577f779056818a8ed4
Benoit <benoit.guchet@gmail.Com>
2025-05-04 18:59:10 +0200

    Zip

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -20,6 +20,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use \ZipArchive;
 
 class DownloadController extends AbstractController
 {
