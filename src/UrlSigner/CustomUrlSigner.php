2b97cf860c4995db606c48a43fada74050b62200
Benoit <benoit.guchet@gmail.Com>
2025-05-05 15:06:05 +0200

    Signature plus simple

---
+++ src/UrlSigner/CustomUrlSigner.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\UrlSigner;
+
+use CoopTilleuls\UrlSignerBundle\UrlSigner\AbstractUrlSigner;
+
+class CustomUrlSigner extends AbstractUrlSigner
+{
+    public static function getName(): string
+    {
+        return 'simpler_signer';
+    }
+
+    protected function createSignature(string $url, string $expiration, string $signatureKey): string
+    {
+        return substr(hash_hmac('sha256', "{$url}::{$expiration}", $signatureKey), 0, 10);
+    }
+}
