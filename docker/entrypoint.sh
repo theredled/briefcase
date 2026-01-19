a52497e71d54cb2be1302b765546007aacd1043d
Benoit <benoit.guchet@gmail.Com>
2026-01-19 09:22:38 +0100

    Dockerisation / 2

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,12 @@
 # Crée le dossier si nécessaire et fixe les permissions
 #mkdir -p /var/www/var
 #cd /var/www/
-chown -R www-data:www-data /var/www/var
+chown -R www-data:www-data /var/www/briefcase/var
+
+rm -rf /var/www/briefcase/var/cache/*
 # Install dépendances
 #echo COMPOSERRRRRR
-#composer install --no-dev --optimize-autoloader --no-scripts
+composer install
 
 # Lance la commande d'origine (PHP-FPM)
 exec "$@"
